5e400eed79575c97eaf787f8458cbff74065960c
高橋 啓治郎 <keijiro@Keijiros-MacBook-Air.local>
2011-09-22 18:56:10 +0900

    サーバー側負荷軽減のための抜本的な対策を行った。

--- GAE/main.py
+++ GAE/main.py
@@ -8,59 +8,69 @@ from google.appengine.api import memcache
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 
-# データの生存期限。
-doodler_expire = 30.0
+# 接続の生存期限。
+con_expire = 30.0
 
-class Matchmaker(db.Model):
+# グループ作成用のダミーモデル。
+class Entry(db.Model):
     None
 
+# マッチング情報。
 class Match(db.Model):
     his_uid = db.StringProperty()
 
+# ストローク情報。
 class Stroke(db.Model):
     data = db.TextProperty()
 
-class Timestamp(db.Model):
+# 最後通信情報。
+class LastAccess(db.Model):
     dateTime = db.DateTimeProperty()
 
-def match_pair(matchmaker):
-    pair = Match.all().ancestor(matchmaker).filter('his_uid =', 'none').fetch(2)
-    if len(pair) > 1:
-        pair[0].his_uid = pair[1].key().name()
-        pair[1].his_uid = pair[0].key().name()
-        db.put(pair)
-
+# マッチング処理。
 class MatchHandler(webapp.RequestHandler):
-    '''マッチング処理'''
     def post(self):
         # 引数の取得。
         uid = self.request.get('uid')
         if not uid:
             self.response.out.write('invalid')
             return
-        #
-        matchmaker = Matchmaker.get_or_insert('global')
-        Match(parent = matchmaker, key_name = uid, his_uid = 'none').put()
-        Stroke(key_name = uid).put()
-        #
-        db.run_in_transaction(match_pair, matchmaker)
-        #
+        # マッチング候補に入れる。
+        match_entry = Entry.get_or_insert('match')
+        Match(parent = match_entry, key_name = uid, his_uid = 'none').put()
+        # UIDベースの情報の初期化を行う。
+        user_entry = Entry.get_or_insert(uid)
+        def txn():
+            stroke = Stroke(parent = user_entry, key_name = uid)
+            last = LastAccess(parent = user_entry, key_name = uid, dateTime = datetime.now())
+            db.put((stroke, last))
+        db.run_in_transaction(txn)
+        # とりあえず成功を返しておく。
         self.response.out.write('ok')
 
+# マッチ相手の取得。
 class GetMateHandler(webapp.RequestHandler):
-    '''マッチ相手の取得'''
     def post(self):
         # 引数の取得。
         uid = self.request.get('uid')
         if not uid:
             self.response.out.write('invalid')
             return
+        # 適当にペアを作ってマッチさせる。
+        match_entry = Entry.get_or_insert('match')
+        def txn():
+            pair = Match.all().ancestor(match_entry).filter('his_uid =', 'none').fetch(2)
+            if len(pair) > 1:
+                pair[0].his_uid = pair[1].key().name()
+                pair[1].his_uid = pair[0].key().name()
+                db.put(pair)
+        db.run_in_transaction(txn)
         # 相手のUIDをレスポンスとして返す。
-        match = Match.get_by_key_name(uid, Matchmaker.get_by_key_name('global'))
+        match = Match.get_by_key_name(uid, match_entry)
         self.response.out.write(match.his_uid if match else 'invalid')
 
+# ストロークの取得。
 class GetStrokeHandler(webapp.RequestHandler):
-    '''ストロークの取得'''
     def post(self):
         # 引数の取得。
         uid = self.request.get('uid')
@@ -68,11 +78,11 @@ class GetStrokeHandler(webapp.RequestHandler):
             self.response.out.write('invalid')
             return
         # ストロークをレスポンスとして返す。
-        stroke = Stroke.get_by_key_name(uid)
+        stroke = Stroke.get_by_key_name(uid, Entry.get_by_key_name(uid))
         self.response.out.write(stroke.data if stroke else 'invalid')
 
+# ストロークの更新。
 class UpdateStrokeHandler(webapp.RequestHandler):
-    '''ストロークの更新'''
     def post(self):
         # 引数の取得。
         uid = self.request.get('uid')
@@ -80,13 +90,18 @@ class UpdateStrokeHandler(webapp.RequestHandler):
         if not uid or not data:
             self.response.out.write('invalid')
             return
-        # ストロークの更新。
-        Stroke(key_name = uid, data = data).put()
+        # 情報の更新。
+        user_entry = Entry.get_by_key_name(uid)
+        def txn():
+            stroke = Stroke(parent = user_entry, key_name = uid, data = data)
+            last = LastAccess(parent = user_entry, key_name = uid, dateTime = datetime.now())
+            db.put((stroke, last))
+        db.run_in_transaction(txn)
         # 成功メッセージを返す。
         self.response.out.write('ok')
 
+# 切断処理。
 class QuitHandler(webapp.RequestHandler):
-    '''切断処理'''
     def post(self):
         # 引数の取得。
         uid = self.request.get('uid')
@@ -94,13 +109,13 @@ class QuitHandler(webapp.RequestHandler):
             self.response.out.write('invalid')
             return
         # 削除。
-        match = Match.get_by_key_name(uid, Matchmaker.get_by_key_name('global'))
+        match = Match.get_by_key_name(uid, Entry.get_by_key_name('match'))
         if match: match.delete();
         # ともかく成功メッセージを返す。
         self.response.out.write('ok')
 
+# トップページのハンドラー。
 class MainHandler(webapp.RequestHandler):
-    '''トップページのハンドラー'''
     def get(self):
         # ダミーメッセージ。
         self.response.out.write('ready')
---
+++ test.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+# BASE_URL="http://localhost:8080"
+BASE_URL="http://gaedoodler.appspot.com"
+
+testcase () {
+    command="curl -s -d uid=$2&data=$3 $BASE_URL/$1"
+    /bin/echo -n $command "=> "
+
+    result=`$command`
+    echo $result
+
+    if [ "$4" != "$result" ]; then
+        echo "Assertion failed."
+        exit 0
+    fi
+}
+
+testcase quit test1 none ok
+testcase quit test2 none ok
+
+testcase mate test1 none invalid
+testcase mate test2 none invalid
+
+testcase match test1 none ok
+testcase match test2 none ok
+
+testcase mate test1 none test2
+testcase mate test2 none test1
+
+testcase update test1 abc ok
+testcase update test2 xyz ok
+
+testcase stroke test1 none abc
+testcase stroke test2 none xyz
+
+testcase update test1 123 ok
+testcase update test2 456 ok
+
+testcase stroke test1 none 123
+testcase stroke test2 none 456
+
+testcase update test1 1 ok
+testcase update test1 2 ok
+testcase update test1 3 ok
+testcase update test2 4 ok
+testcase update test2 5 ok
+testcase update test2 6 ok
+
+testcase stroke test1 none 3
+testcase stroke test2 none 6
+
+testcase quit test1 none ok
+testcase mate test1 none invalid
+testcase mate test2 none test1
+
+testcase quit test2 none ok
+testcase mate test2 none invalid
+
+echo "Successfully ended."
